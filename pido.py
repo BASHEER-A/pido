84fbaa188ddd602a6552f50b0d2a6ea0c8d1ccb7
BASHEER AHMED <irxx@live.com>
2022-02-26 17:31:51 +0300

    Update pido.py
    multiprocessing

--- pido.py
+++ pido.py
@@ -9,7 +9,7 @@ from ctypes import c_int
 import tempfile
 from sys import argv
 
-import dummy
+from multiprocessing import dummy
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0', 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Accept-Language': 'en-US,en;q=0.5','Accept-Encoding': 'gzip, deflate, br','Connection': 'keep-alive','Range':''}
 timeout = 30
 
